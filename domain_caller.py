0455a7dc2d01429efeae718cb1a34a0df6a39bf6
gspracklin <george.spracklin@gmail.com>
2018-01-03 13:59:14 -0500

    Update domain_caller.py

--- domain_caller.py
+++ domain_caller.py
@@ -1,7 +1,7 @@
 """
 domain_caller.py is designed to identify broad chromatin domain on the megabase scale.
 
-v1.0
+v2.0
 """
 import os, sys, getopt
 
@@ -9,9 +9,10 @@ def main(argv):
     inputfile = ''
     outputfile = ''
     try:
-        opts, args = getopt.getopt(argv,"hi:o:w:",["input=","output=","window="])
+        opts, args = getopt.getopt(argv,"hi:o:w:c:",["input=","output=","window=","chromosome="])
     except getopt.GetoptError:
-        print ('domail_caller.py -i <inputfile> -o <outputfile> -w <window size>')
+        print('Input Error:')
+        print ('domail_caller.py -i <inputfile> -o <outputfile> -w <window size> -c <chromosome>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -25,48 +26,96 @@ def main(argv):
             outputfile = arg
         elif opt in ("-w", "--window"):
             win_size = int(arg)
-#        elif opt in ("-c", "--chromosome"):
-#            chromosome = arg
+        elif opt in ("-c", "--chromosome"):
+            chromosome = arg
     print ('Input file is:', inputfile)
     print ('Output file is:', outputfile)
     print ('Window size: ', win_size)
-#    print ('chromosome: ', chromosome)
-    domain_caller(inputfile, outputfile, win_size)
-#    os.remove(outputfile)
-    print('All finished')
+    print ('chromosome: ', chromosome)
+    data = chr_split(inputfile)
+
+    #Loop for all chromosomes
+    if chromosome == 'all':
+        mylist=[item[0] for item in data]
+        mylist2=list(set(mylist))
+        for item in sorted(mylist2):
+            lyst=chromo(data, item)
+            domain_caller(lyst, outputfile, win_size)
+    #For an individual chromosomes
+    else:
+        lyst = chromo(data, chromosome)
+        domain_caller(lyst, outputfile, win_size)
+
+def chromo(data, chromosome):
+    """Returns a chromosome specific list"""
+    lyst=[]
+    for i in range(0,len(data),1):
+        if chromosome == str(data[i][0]):
+            lyst.append(data[i])
+    return lyst
 
 
-########################################
-#Main Program to call broad domains
-########################################
-def domain_caller(inputfile, outputfile, win_size=100):
-    threshold=int(win_size/10)
-    print('the window size is: ', win_size)
-    print('the threshold is: ', threshold)
-    output_file=open(outputfile,'w')
+def chr_split(inputfile):
+    """Takes bedgraph and outputs dataframe"""
     with open(inputfile,'r') as f:
       data=f.readlines()
     f.close()
-    data=[x.strip('\n').split() for x in data]
+    data=[x.strip('\n').split() for x in data ]
+    return data
+
+
+def domain_caller(lyst, outputfile, win_size=100):
+    """Meat and Potatoes"""
+    if win_size < 5:
+        print('Window size is too small for optimal use')
+        return
+    threshold=float(win_size/10) + 0.5
+    print('Starting chr', lyst[0][0] )
+    output_file=open(outputfile,'a')
     loop=1
-    print('starting for loop')
-    for i in range(0,len(data),1):
-        if (loop == 1 and float(data[i][3]) > 0): #will be first column of bedfile
-            item=0
+    look_thru = 3 #this variable will allow stocastic negative values to be skipped
+    for i in range(0,len(lyst),1):
+        #Find the start of a domain
+        if (loop == 1 and float(lyst[i][3]) > 0): #will be first column of bedfile
+            item=-1
             holder=[]
+
+            #Create list and check if domain is large
             while item < win_size:
-                holder.append(float(data[i+item][3]))
                 item+=1
-            if (sum(n<0 for n in holder)) == threshold:
-                start=data[i][0]+'\t'+data[i][1]+'\t'
+                try:
+                    holder.append(float(lyst[i+item][3]))
+                except IndexError:
+                    continue #might want to use Pass here
+            #write start of large domain to file
+            if (sum(n<0 for n in holder)) <= threshold:
+                start=lyst[i][0]+'\t'+lyst[i][1]+'\t'
                 output_file.write(start)
                 loop=0
-        if (loop == 0 and float(data[i][3]) >0):
+
+        #Start domain already started, keep going till negative value reached
+        if (loop == 0 and float(lyst[i][3]) >0):
             continue
-        if (loop == 0 and float(data[i][3]) <0): #ends domain and adds third column to bedfile
-            end=data[i-1][2]
-            output_file.write(end+'\n')
-            loop=1
+
+        #Find End of domain
+        if (loop == 0 and float(lyst[i][3]) <0):
+            diff_item=-1
+            diff_holder=[]
+            while diff_item < look_thru:
+                diff_item+=1
+                try:
+                    diff_holder.append(float(lyst[i+diff_item][3]))
+                except IndexError:
+                    continue #might want to use Pass here
+            if (sum(n<0 for n in diff_holder)) <= threshold:
+                continue
+            elif (sum(n<0 for n in diff_holder)) > threshold:
+                end=lyst[i-1][2]
+                output_file.write(end+'\n')
+                loop=1
+    #Add end of domain if lyst ends on row of positives
+    if loop == 0:
+        output_file.write(lyst[i][2]+'\n')
     output_file.close()
 
 if __name__ == "__main__":
